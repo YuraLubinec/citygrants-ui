480dd689f7b8235c18c2fd33a0033bb6c8f1be8e
Dmytro Andrusiv <dmytro.andrusiv@gmail.com>
2017-11-30 10:32:20 +0200

    added service for jury page

--- src/app/jury-page/jury-page.component.ts
+++ src/app/jury-page/jury-page.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { JuryService } from '../services/jury.service';
+import { ProjectApplication } from '../models/projectApplication';
 
 @Component({
   selector: 'app-jury-page',
   templateUrl: './jury-page.component.html',
   styleUrls: ['./jury-page.component.css'],
-  providers: []
+  providers: [JuryService]
 })
 
 export class JuryPageComponent implements OnInit {
+
+  private projects : Array<ProjectApplication>;
+
+  constructor(private juryService: JuryService) {
+    this.juryService.getAllProjects().subscribe(data => this.dataHandler(data),this.searchErrorHandler);
+  
+  }
+
   ngOnInit(): void {}
+
+  private dataHandler(projects: any){
+    this.projects = projects as Array<ProjectApplication>;
+    console.log(projects);
+  }
+
+  private searchErrorHandler(error: any) {
+    console.log(error);
+    alert("Вході виконання програми виникла помилка, спробуйте пізніше");
+  }
 }
---
+++ src/app/services/jury.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ProjectApplication } from '../models/projectApplication';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class JuryService {
+
+  private baseUrl: string;
+
+
+  constructor(private http: HttpClient) {
+
+    this.baseUrl = 'http://localhost:8082/citygrants/client/project';
+  }
+
+  getAllProjects(): Observable<any> {
+    console.log("service running")
+    return this.http.get(this.baseUrl);
+  }
+
+  private handlePromiseError(err): Promise<any> {return Promise.reject(err);}
+}
