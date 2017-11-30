af3710e33d44651a891da824b38396ce163c735c
Dmytro Andrusiv <dmytro.andrusiv@gmail.com>
2017-11-30 16:14:29 +0200

    added table with projects with jury

--- src/app/jury-page/jury-page.component.ts
+++ src/app/jury-page/jury-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { JuryService } from '../services/jury.service';
 import { ProjectApplication } from '../models/projectApplication';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-jury-page',
@@ -12,17 +13,18 @@ import { ProjectApplication } from '../models/projectApplication';
 export class JuryPageComponent implements OnInit {
 
   private projects : Array<ProjectApplication>;
+  private displayedColumns = ['nameOfProject', 'requestedBudget', 'organizationName', 'theme','goal'];
+  private dataSource : any;
 
   constructor(private juryService: JuryService) {
     this.juryService.getAllProjects().subscribe(data => this.dataHandler(data),this.searchErrorHandler);
-  
   }
 
   ngOnInit(): void {}
 
   private dataHandler(projects: any){
     this.projects = projects as Array<ProjectApplication>;
-    console.log(projects);
+    this.dataSource = new MatTableDataSource(this.projects);
   }
 
   private searchErrorHandler(error: any) {
