87c55b43a705a9d5518ec71b7aeca9f67fe4eb82
Dmytro Andrusiv <dmytro.andrusiv@gmail.com>
2018-01-03 11:18:48 +0200

    added model of comment

---
+++ src/app/models/comment.ts
@@ -0,0 +1,11 @@
+export class Comment{
+    userId:String;
+    userName:String;
+    text:String;
+
+    constructor(userId?:String, userName?:String, text?:String){
+        this.userId = userId;
+        this.userName = userName;
+        this.text = text;
+    }
+}
