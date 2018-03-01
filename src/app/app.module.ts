097281462225e064b5a964564c9573ad6658ba16
Unknown <yuralubinec@gmail.com>
2018-03-01 15:54:23 +0200

    removed syntax mistakes in app.module after merging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatTableModule, MatSortModule, MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSelectModule,
-  MatButtonModule, MatIconModule, MatDialogModule, MatToolbarModule, MatCardModule, MatListModule, MatSliderModule,
-  MatExpansionModule, MatStepperModule, MatSnackBarModule, MatGridListModule, MatCheckboxModule, MatTabsModule
-} from '@angular/material';
 import { AppComponent } from './app.component';
 import { ClientPageComponent } from './client-page/client-page.component';
 import { JuryPageComponent } from "./jury-page/jury-page.component";
 import { JuryDialogPageComponent } from './project-dialog-page/project-dialog-page.component';
 import { RoutingModule } from "./routing/routing.module";
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatTableModule, MatSortModule, MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSelectModule,
   MatButtonModule, MatIconModule, MatDialogModule, MatToolbarModule, MatCardModule, MatListModule, MatSliderModule,
-  MatExpansionModule,MatStepperModule, MatSnackBarModule, MatGridListModule, MatCheckboxModule,MatSlideToggleModule
+  MatExpansionModule, MatStepperModule, MatSnackBarModule, MatGridListModule, MatCheckboxModule, MatSlideToggleModule
 } from '@angular/material';
 import { MatTabsModule } from '@angular/material';
 import { AdminPageComponent } from './admin-page/admin-page.component';
@@ -43,7 +36,7 @@ import { LocalStorageService } from './services/locastorage.service';
     BrowserModule, RoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule, BrowserAnimationsModule,
     MatTableModule, MatSortModule, MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSelectModule, MatTabsModule,
     MatInputModule, MatSelectModule, MatDialogModule, MatButtonModule, MatToolbarModule, MatCardModule, MatListModule, MatSliderModule, MatExpansionModule,
-    MatIconModule, MatStepperModule, MatSnackBarModule, MatGridListModule,MatCheckboxModule, MatSlideToggleModule
+    MatIconModule, MatStepperModule, MatSnackBarModule, MatGridListModule, MatCheckboxModule, MatSlideToggleModule
   ],
   providers: [
     {
