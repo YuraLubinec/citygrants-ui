f9dc1381db99a66af62268bc26084d4cd7a06606
Dmytro Andrusiv <hfl-if@ukr.net>
2018-04-27 11:47:54 +0300

    changed style of comments and downloaded files

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -20,7 +20,7 @@ export class AdminPageComponent implements OnInit {
   private pageSize   : Number;
   private pageEvent  : PageEvent;
   private pageSizeOptions  = [5, 10, 25, 50];
-  private displayedColumns = ['select', 'nameOfProject', 'requestedBudget', 'organizationName', 'theme','goal','totalEvalFirstStage','totalEvalSecondStage','buttons'];
+  private displayedColumns = ['nameOfProject', 'requestedBudget', 'organizationName', 'theme','goal','totalEvalFirstStage','totalEvalSecondStage','select', 'buttons'];
   private selection  : SelectionModel<ProjectAdm>;
   private positionTollTip = "above";
   
